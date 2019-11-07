4699a378a69a2597864e3407866724cbbfcaff75
Leandro Simões <leandro.simoes@outlook.com>
2019-11-07 11:25:07 -0300

    Included reactotron package

---
+++ reactotron-package/ReadMe.md
@@ -0,0 +1,3 @@
+﻿# Reactotron (Non Official) Chocolatey Package
+
+This package is not official, but it will instlal reactotron from the official github repository, don't worry.
---
+++ reactotron-package/tools/chocolateyinstall.ps1
@@ -0,0 +1,6 @@
+﻿$packageName = 'reactotron'
+$installerType = 'msi' 
+$url = 'https://github.com/infinitered/reactotron/releases/download/v2.17.1/Reactotron.2.17.1.msi' 
+$silentArgs = '/quiet' 
+
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url"
