0154f6074add838308334ca3480350f52ec4a75d
Leandro Simões <leandro.simoes@outlook.com>
2019-12-02 09:48:00 -0300

    fixes

--- jaime-package/tools/chocolateyinstall.ps1
+++ jaime-package/tools/chocolateyinstall.ps1
@@ -1,10 +1,5 @@
 ﻿$packageName = 'Jaime'
+$checksum = '5539F6075976B69B43125784A41E6DAB4635DFBEF3A10FB2D0DA1E24F41972E9'
 $url = 'https://github.com/leandrosimoes/Jaime/releases/download/v2.0.2/Jaime.zip' 
 
-Install-ChocolateyZipPackage -PackageName "$packageName" `
- -Url "$url" `
- -UnzipLocation "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
-
-if ($?) {            
-    iex "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)\\jaime.exe"
-}
+Install-ChocolateyZipPackage -PackageName "$packageName" -Url "$url" -UnzipLocation "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)" -ChecksumType 'sha256' -Checksum $checkSum
--- reactotron-package/tools/chocolateyinstall.ps1
+++ reactotron-package/tools/chocolateyinstall.ps1
@@ -1,6 +1,7 @@
 ﻿$packageName = 'reactotron'
+$checksum = 'F8292498B15B81D236AB6983857960F228325CF3FB682A0F670B496C31117FD3'
 $installerType = 'msi' 
 $url = 'https://github.com/infinitered/reactotron/releases/download/v2.17.1/Reactotron.2.17.1.msi' 
-$silentArgs = '/quiet' 
+$silentArgs = '/quiet'
 
-Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url"
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" -ChecksumType "sha256" -Checksum $checkSum
